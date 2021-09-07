5d976979429b79a4592dd93e37941cb58576b1f5
Lucas van Pelt <lucasvpelt03@gmail.com>
2021-09-07 14:16:18 +0200

    Scripts van de berekeningen

---
+++ feestlunch.py
@@ -0,0 +1,8 @@
+Croissant = 17
+CroissantPrijs = 0.39
+Stokbrood = 2
+StokbroodPrijs = 2.78
+Kortingsbon = 3
+KortingsbonPrijs = 0.50
+
+print(Croissant * CroissantPrijs + (Stokbrood * StokbroodPrijs) - (Kortingsbon * KortingsbonPrijs))
---
+++ speelhaldag.py
@@ -0,0 +1,7 @@
+Toegangsticket = 7.45
+Personen = 3
+GameSeatMinuten = 5
+GameSeatTotaal = 45
+GameSeatPrijs = 0.37
+
+print(Toegangsticket * Personen + (GameSeatTotaal / GameSeatMinuten * GameSeatPrijs))
