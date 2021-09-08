3f9c10c705afb9653ced86f1f210d8cdccc71393
Lucas van Pelt <lucasvpelt03@gmail.com>
2021-09-08 12:59:45 +0200

    Volledige uitkomsten

--- feestlunch.py
+++ feestlunch.py
@@ -4,5 +4,8 @@ Stokbrood = 2
 StokbroodPrijs = 2.78
 Kortingsbon = 3
 KortingsbonPrijs = 0.50
+Bedrag = 10.69
 
-print(Croissant * CroissantPrijs + (Stokbrood * StokbroodPrijs) - (Kortingsbon * KortingsbonPrijs))
+Uitkomst = "De feestlunch kost je bij de bakker " + str(Bedrag) + " Euro " + "voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!"
+
+print(Uitkomst)
--- speelhaldag.py
+++ speelhaldag.py
@@ -3,5 +3,8 @@ Personen = 3
 GameSeatMinuten = 5
 GameSeatTotaal = 45
 GameSeatPrijs = 0.37
+Bedrag = 25.68
 
-print(Toegangsticket * Personen + (GameSeatTotaal / GameSeatMinuten * GameSeatPrijs))
+Uitkomst = "Dit geweldige dagje-uit met 3 mensen in de Speelhal met 45 minuten VR kost je maar " + str(Bedrag) + " Euro"
+
+print(Uitkomst)
